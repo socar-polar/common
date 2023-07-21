d125c8135a65758806cb982d2221a8156dac117c
socar-polar <polar@socar.kr>
2023-07-21 11:53:11 +0900

    feat: create repository

---
+++ src/main/kotlin/com/polar/polarcommon/PolarCommonApplication.kt
@@ -0,0 +1,11 @@
+package com.polar.polarcommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolarCommonApplication
+
+fun main(args: Array<String>) {
+	runApplication<PolarCommonApplication>(*args)
+}
